c34fada6ec1ec93e05dd4939678994a2b04ca887
Naman-Karara <147559148+Naman-Karara@users.noreply.github.com>
2023-11-17 02:19:47 +0530

    Add files via upload

---
+++ olx_latest-main/includes/config.php
@@ -0,0 +1,18 @@
+<?php
+
+//Site
+$siteURL = "http://localhost/olx";
+$siteName = "olx";
+
+//DB 
+$dbHost = 'localhost';
+$dbUser = 'root';
+$dbPass = 'karara1234';
+$dbName = 'olx';
+
+//General Settings 
+$defaultLan = 'en';
+$defaultThemeColor = 'blue';
+
+
+?>
